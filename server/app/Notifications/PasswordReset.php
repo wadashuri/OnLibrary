b7a8383805836a03e3c56064857284fea046a615
wadashuri <wada20010108@icloud.com>
2022-10-23 03:11:10 +0900

    no message

--- server/app/Notifications/PasswordReset.php
+++ server/app/Notifications/PasswordReset.php
@@ -43,8 +43,8 @@ class PasswordReset extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-        ->subject(Lang::get('Reset Password Notification'))
-        ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+        ->subject(Lang::get('パスワードリセット申請'))
+        ->line(Lang::get('ログインのパスワードリセットの申請を受け付けました。'))
         ->action(Lang::get('Reset Password'), url('password/reset', $this->token))
         ->line(Lang::get('このパスワードは :count 分後に期限切れとなります。', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
         ->line(Lang::get('If you did not request a password reset, no further action is required.'));
