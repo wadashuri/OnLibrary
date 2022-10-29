e576e846745dc4154e88c3043f4679f4c9082728
wadashuri <wada@20010108@icloud.com>
2022-10-29 16:08:23 +0900

    修正

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -30,104 +30,136 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
-	<!-- Google tag (gtag.js) -->
+    <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HRK5SDJKJ1"></script>
     <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-HRK5SDJKJ1');
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-HRK5SDJKJ1');
     </script>
 </head>
 
 <body>
     <div id="app">
-        @if(empty(strstr(url()->current(),'search')))
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('home') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <a href="{{ route('search.index') }}">
-                    <div><img alt="" height="26"
-                            src="{{ asset('/img/common/search.svg') }}" width="25"></div>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('home') }}">
-                                ホーム
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('likes.index') }}">
-                                ライブラリ
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+        @if (empty(strstr(url()->current(), 'search')))
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('home') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <a href="{{ route('search.index') }}">
+                        <div><img alt="" height="26" src="{{ asset('/img/common/search.svg') }}"
+                                width="25"></div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('home') }}">
+                                    ホーム
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    {{-- あとで実装 --}}
-                                    {{-- <a class="dropdown-item" href="{{ route('categories.index') }}">
-                                        マイページ
-                                    </a> --}}
-                                    <a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;" href="{{ url('/service/privacy-policy/') }}" target="_blank" rel="noopener noreferrer">
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('likes.index') }}">
+                                    ライブラリ
+                                </a>
+                            </li>
+                        </ul>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                        href="{{ url('/service/privacy-policy/') }}" target="_blank"
+                                        rel="noopener noreferrer">
                                         プライバシーポリシー
                                     </a>
-                                    <a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;" href="{{ url('/service/お問い合わせ/') }}" target="_blank" rel="noopener noreferrer">
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                        href="{{ url('/service/お問い合わせ/') }}" target="_blank" rel="noopener noreferrer">
                                         お問合わせ
                                     </a>
-                                    <a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;" href="{{ url('/service/onlibraryの使い方/') }}" target="_blank" rel="noopener noreferrer">
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                        href="{{ url('/service/onlibraryの使い方/') }}" target="_blank"
+                                        rel="noopener noreferrer">
                                         OnLibraryとは
                                     </a>
-                                    @can('isAdmin', Auth::user())
-                                    <a class="dropdown-item" href="{{ route('posts.index') }}">
-                                        管理者画面
-                                    </a>
-                                    @endcan
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                </li>
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        {{-- あとで実装 --}}
+                                        {{-- <a class="dropdown-item" href="{{ route('categories.index') }}">
+                                        マイページ
+                                    </a> --}}
+                                        <a class="dropdown-item"
+                                            onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                            href="{{ url('/service/privacy-policy/') }}" target="_blank"
+                                            rel="noopener noreferrer">
+                                            プライバシーポリシー
+                                        </a>
+                                        <a class="dropdown-item"
+                                            onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                            href="{{ url('/service/お問い合わせ/') }}" target="_blank" rel="noopener noreferrer">
+                                            お問合わせ
+                                        </a>
+                                        <a class="dropdown-item"
+                                            onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
+                                            href="{{ url('/service/onlibraryの使い方/') }}" target="_blank"
+                                            rel="noopener noreferrer">
+                                            OnLibraryとは
+                                        </a>
+                                        @can('isAdmin', Auth::user())
+                                            <a class="dropdown-item" href="{{ route('posts.index') }}">
+                                                管理者画面
+                                            </a>
+                                        @endcan
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         @endif
         <main class="py-4">
             @yield('content')
