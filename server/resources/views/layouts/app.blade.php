2ab2e46d72bfdbd72658f54b7ac6958561095762
wadashuri <wada@20010108@icloud.com>
2022-11-13 15:12:18 +0900

    syuusei

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title')</title>
 
     <!-- scripts-->
     <script src="{{ mix('js/app.js') }}" defer></script>
--- server/resources/views/likes/show.blade.php
+++ server/resources/views/likes/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', $title)
+@section('title', $post->title)
 
 @section('content')
     <div class="container">
