f2f22cc2f8d0c3cafb6b7b514989ae0b53b84106
wadashuri <wada20010108@icloud.com>
2023-01-20 22:48:58 +0900

    修正しました

--- server/resources/views/ajaxaddpost.blade.php
+++ server/resources/views/ajaxaddpost.blade.php
@@ -5,7 +5,7 @@
                 <div class="card h-100">
                     <div class="ratio ratio-16x9">
                         <img src="{{ str_replace('youtu.be/', 'i.ytimg.com/vi/', $post->video) }}/hqdefault.jpg"
-                            alt="{{ $post->title }}">
+                            alt="{{ $post->title }}"  class="h-100" style="object-fit: cover;">
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -17,7 +17,7 @@
                                 <div class="card h-100">
                                     <div class="ratio ratio-16x9">
                                         <img src="{{ str_replace('youtu.be/', 'i.ytimg.com/vi/', $order_like->video) }}/hqdefault.jpg"
-                                            alt="{{ $order_like->title }}">
+                                            alt="{{ $order_like->title }}" class="h-100" style="object-fit: cover;">
                                     </div>
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $order_like->title }}</h5>
@@ -45,7 +45,7 @@
                             <div class="card h-100">
                                 <div class="ratio ratio-16x9">
                                     <img src="{{ str_replace('youtu.be/', 'i.ytimg.com/vi/', $post->video) }}/hqdefault.jpg"
-                                        alt="{{ $post->title }}">
+                                        alt="{{ $post->title }}"  class="h-100" style="object-fit: cover;">
                                 </div>
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $post->title }}</h5>
