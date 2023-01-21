e78e56af42ba1aa461173a019b99160d89d28d3b
wadashuri <wada20010108@icloud.com>
2023-01-21 20:41:40 +0900

    上のライブラリへ移動の時確認画面を出すように変更。 あとで関数化したほうがいいかも

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -67,10 +67,18 @@
                             <a class="nav-link text-black" href="{{ route('search.index') }}"><i
                                     class="bi bi-search"></i>検索</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-black" href="{{ route('likes.index') }}"><i
-                                    class="bi bi-book"></i>ライブラリ</a>
-                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link text-black" href="{{ route('likes.index') }}"
+                                    onclick="if(!confirm('ログインが必要です、移動しますか？')) return false;"><i
+                                        class="bi bi-book"></i>ライブラリ</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link text-black" href="{{ route('likes.index') }}"><i
+                                        class="bi bi-book"></i>ライブラリ</a>
+                            </li>
+                        @endguest
                     </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
@@ -113,10 +121,6 @@
                                     {{ Auth::user()->name }}
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    {{-- あとで実装 --}}
-                                    {{-- <a class="dropdown-item" href="{{ route('categories.index') }}">
-                                        マイページ
-                                    </a> --}}
                                     <a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
                                         href="{{ url('/service/privacy-policy/') }}" target="_blank"
                                         rel="noopener noreferrer">
@@ -174,15 +178,16 @@
                 <li class="nav-item">
                     @guest
                         <a class="nav-link p-1 py-3 link-secondary {{ Request::routeIs('likes.index') ? 'active' : '' }}"
-                        onclick="if(!confirm('この機能は会員登録が必要です、移動しますか？')) return false;" href="{{ route('likes.index') }}"><i class="bi bi-book" style="color: gray;"></i>ライブラリ</a>
+                            onclick="if(!confirm('ログインが必要です、移動しますか？')) return false;"
+                            href="{{ route('likes.index') }}"><i class="bi bi-book" style="color: gray;"></i>ライブラリ</a>
                     @else
                         <a class="nav-link p-1 py-3 link-secondary {{ Request::routeIs('likes.index') ? 'active' : '' }}"
                             href="{{ route('likes.index') }}"><i class="bi bi-book" style="color: gray;"></i>ライブラリ</a>
                     @endguest
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link p-1 py-3 dropdown-toggle link-secondary" data-bs-toggle="dropdown" href="#"
-                        role="button" aria-expanded="false"><i class="bi bi-door-open"
+                    <a class="nav-link p-1 py-3 dropdown-toggle link-secondary" data-bs-toggle="dropdown"
+                        href="#" role="button" aria-expanded="false"><i class="bi bi-door-open"
                             style="color: gray;"></i>会員登録</a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="{{ route('register') }}"><i
@@ -195,12 +200,20 @@
                         <li><a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
                                 href="{{ url('/service/onlibraryの使い方/') }}"><i class="bi bi-card-text"></i>
                                 OnLibraryとは</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
                         <li><a class="dropdown-item" onclick="if(!confirm('外部サイトへ移動します、よろしいですか？')) return false;"
                                 href="{{ url('/service/お問い合わせ/') }}"><i class="bi bi-chat-right-text"></i>
                                 お問合わせ</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                <i class="bi bi-box-arrow-left"></i>
+                                {{ __('Logout') }}
+                            </a>
+                        </li>
                     </ul>
                 </li>
             </ul>
