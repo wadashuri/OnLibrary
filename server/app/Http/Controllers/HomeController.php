8646bdfa74c92593e73e01c5d413cd83a2695245
wadashuri <wada20010108@icloud.com>
2023-01-29 16:06:18 +0900

    ホームコントローラーを整理

--- server/app/Http/Controllers/HomeController.php
+++ server/app/Http/Controllers/HomeController.php
@@ -22,17 +22,10 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $posts = Post::with('categories', 'user', 'likes')->orderBy('created_at', 'desc')->take(6)->get();
-
-        $likes = new Like;
-
-        $order_likes = Post::withCount('likes')->with('categories', 'user', 'likes')->orderBy('likes_count', 'desc')->take(5)->get();
-
         return view('home', [
-            'posts' =>  $posts,
-            'likes' => $likes,
             'title' => 'Home',
-            'order_likes' => $order_likes,
+            'posts' =>  Post::with('categories', 'user', 'likes')->orderBy('created_at', 'desc')->take(6)->get(),
+            'order_likes' => Post::withCount('likes')->with('categories', 'user', 'likes')->orderBy('likes_count', 'desc')->take(5)->get()
         ]);
     }
 }
